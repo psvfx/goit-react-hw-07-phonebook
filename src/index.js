2e2167e0eb852c35dc2ec0a393c74be6d3b7ee00
psvfx <psvfxinvest@gmail.com>
2023-09-30 15:02:21 +0300

    Added project folder and file structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App/App';
+import { GlobalStyle } from './components/styleTheme/GlobalStyle';
+import { theme } from './components/styleTheme/theme';
+import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-07-phonebook">
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
