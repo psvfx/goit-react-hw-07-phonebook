0e9cd5ae0c534877021b8a2889a3f137d71c53f7
psvfx <psvfxinvest@gmail.com>
2023-09-30 16:54:03 +0300

    Added App and redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,28 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from 'components/Layout/Layout';
+
+const Home = lazy(() => import('../../Pages/Home'));
+const ContactDetails = lazy(() =>
+  import('../../Pages/ContactDetails/ContactDetails')
+);
+const PhoneView = lazy(() => import('../../Pages/PhoneView/PhoneView'));
+const ContactEdit = lazy(() => import('../../Pages/ContactEdit/ContactEdit'));
+const AddContacts = lazy(() => import('../../Pages/AddContacts/AddContacts'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact/:id" element={<ContactDetails />}>
+            <Route index element={<PhoneView />} />
+            <Route path="edit" element={<ContactEdit />} />
+          </Route>
+          <Route path="addContact" element={<AddContacts />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
